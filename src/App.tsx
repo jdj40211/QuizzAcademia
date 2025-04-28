ed4459334f1178dec280fd6decaf27d1245547bc
jdj40211 <jdsierrao@eafit.edu.co>
2025-04-28 18:20:47 -0500

    Implement dynamic resizing and styling for quiz container

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Quiz from './components/Quiz';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center py-8">
+    <div className="quiz-container">
       <Quiz />
     </div>
   );
